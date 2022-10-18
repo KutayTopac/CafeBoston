d67289e045914f937914d9783e2f1d707b7a1f97
kutay <kutaytopac@outlook.com>
2022-10-18 14:03:26 +0300

    README changes and classes added

---
+++ CafeBoston.DATA/Classes/OrderState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeBoston.DATA.Classes
+{
+    public enum OrderState
+    {
+        Active = 0,
+        Paid = 1,
+        Cancelled = 2
+    }
+}
